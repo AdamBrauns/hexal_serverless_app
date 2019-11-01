aa6a2b0c7aeff8f56bdde8ebd95b6c5beea86005
Adam Brauns <braunsaj14@uww.edu>
2019-11-01 14:22:25 -0500

    updated functions to call the REST api

--- src/components/Products.js
+++ src/components/Products.js
@@ -10,9 +10,18 @@ export default class Products extends Component {
     products: []
   }
 
-  fetchProducts = () => {
+  fetchProducts = async () => {
     // add call to AWS API Gateway to fetch products here
     // then set them in state
+    try
+    {
+      const res = await axios.get(`${config.api.invokeUrl}/products`);
+      this.setState({ products: res.data });
+    }
+    catch (err)
+    {
+      console.log(`An error has occurred: ${err}`)
+    }
   }
 
   componentDidMount = () => {
